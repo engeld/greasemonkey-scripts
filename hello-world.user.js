dcb22b9631f2eec610544c71ca84d11a2ed5e909
engeld <engel.dana.mail@gmail.com>
2013-07-08 17:13:27 +0200

    Create hello-world.user.js

---
+++ hello-world.user.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name          Hello World
+// @namespace     engeld.cc
+// @description   Example which prints a "Hello World"
+// @include       *
+// ==/UserScript==
+
+alert('Hello world!');
