39dc7761e8bdefa33a6983e1351ea29d21f053e3
engeld <engel.dana.mail@gmail.com>
2013-07-08 17:11:00 +0200

    Create goodreads.user.js and initialize it with userscript-header

---
+++ goodreads.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name           GoodReads Customization
+// @namespace      engeld.cc
+// @description    Tweakes some annoying things on goodreads.com
+// @include        http://*.goodreads.com/*
+// @include        https://*.goodreads.com/*
+// ==/UserScript==
